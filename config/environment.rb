bba597ed217e496a960a5ce8e1f8216254813aa3
jaspinderkaur <jaspinder.kaur028@gmail.com>
2022-08-19 14:56:08 +0400

    Added sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+  user_name: 'apikey',
+  password: Rails.application.credentials.dig(:sendgrid, :api_key), 
+  domain: 'async-cloud.herokuapp.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
